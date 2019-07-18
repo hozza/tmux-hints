c0a5bea6c4dc307dcf5ed81ec65a233418713c62
hozza <hozza@users.noreply.github.com>
2019-07-18 14:05:17 +0000

    Update: Fixes from yokisuci on Reddit, added header and default to tmux file and init text etc. Quiet mode, improved help.

--- tmux-hints.sh
+++ tmux-hints.sh
@@ -16,26 +16,33 @@
 
 viewer="cat"
 extension=".txt"
-hint_path="~/hints/"
+hint_path="$HOME/hints/"
 verbose=false
+quiet=false
+header_init="# tmux-hints.sh\n"
+repo="https://github.com/hozza/tmux-hints"
+version="0.2"
 
-while getopts 'o:x:p:vh' flag; do
+while getopts 'o:x:p:vhq' flag; do
 	case "${flag}" in
 		o) viewer="${OPTARG}" ;;
 		x) extension=".${OPTARG}" ;;
 		p) hint_path="${OPTARG}" ;;
 		v) verbose=true ;;
+		q) quiet=true ;;
 		h) 
-			echo "# Usage: $@ -o=opener cat|less -x=extension md|txt -p=path /path/to/hint/files/ -v=verbose -h=help"
+			echo -e "$header_init\ntmux-hints.sh version $version Usage:\n\nSynopsis:\n\n\ttmux-hints.sh [SHORT-OPTION]\n\nOption [default] Description:\n\n\t-o\t[cat] hint file opener e.g. less|markdown\n\t-x\t[txt] hint file extension\n\t-p\t[$HOME/hints/] /path/to/hint/files/\n\t-v\tverbose\n\t-q\tquiet mode, hint only\n\t-h\tthis help text\n\nFor more help visit the repo: $repo"
 			exit 2 
 		;;
 		*) break ;;
 	esac
 done
 
-
 temp_file=$( mktemp  )
 trap "rm -f $temp_file" EXIT
+
+# make sure focus hooks are enabled
+tmux set-option focus-events on
 
 tmux set-hook pane-focus-in "run 'echo #{pane_current_command} > $temp_file'"
 
@@ -43,22 +50,66 @@ tmux set-hook pane-focus-in "run 'echo #{pane_current_command} > $temp_file'"
 clear_term="\ec"
 [ "$verbose" == true ] && clear_term=''
 
+default="\nCreate a default hint file:\n\n"$hint_path"default"$extension"\n\nExit:\tCtrl+c\n\nUpdate:\t'git pull' in tmux-hint directory.\n\nHelp:\ttmux-hints.sh -h\n\nRepo:\t$repo\n"
+
+# init
+if [ "$quiet" == true ]; then
+	header_init=""
+	default=""
+fi
+echo -e $clear_term$header_init
+init=true
+
+
 ### Set initial time of file
 LTIME=`stat -c %Z $temp_file`
 
+# loop - checking for changes to tmp file
 while true
 do
 	ATIME=`stat -c %Z $temp_file`
 
-	if [[ "$ATIME" != "$LTIME" ]]
+	if [ "$ATIME" != "$LTIME" ] || [ "$init" == true ]
 	then
 		
-		if [ "$verbose" == true ]; then 
-			echo -e "# Command: $viewer $hint_path$( cat $temp_file )$extension\n\n"
+		this_hint=$( cat $temp_file )
+		echo -ne $clear_term
+
+		if [ "$init" == true ]; then this_hint="tmux"; fi
+		init=false
+
+		# show this hint
+		header="$header_init"
+
+		# verbose full command
+		[ "$verbose" == true  ] && header=$header"# Exact Command Used: $viewer $hint_path$this_hint$extension\n"
+		
+		# hint exists? default hint?
+		hint_exists=true
+		if [ ! -f "$hint_path$this_hint$extension" ]; then
+			
+			# defaults?
+			if [ -f $hint_path"tmux"$extension ]; then
+				header=$header"# $this_hint hint not found.\n"
+				this_hint="tmux"
+			else
+				header=$header"# tmux hint not found.\n"$default
+				hint_exists=false;
+			fi
+
+		fi
+		
+		# output!
+		if [ "$quiet" == false ]; then
+			echo -e "$header# $this_hint$extension\n"
+		fi	
+		
+		if [ "$hint_exists" == true ]; then
+			echo -e "$( $viewer $hint_path$this_hint$extension )"
 		fi
 
-		echo -ne "$clear_term$( $viewer $hint_path$( cat $temp_file )$extension )"
 		LTIME=$ATIME
+		
 	fi
 	sleep 1
 done
